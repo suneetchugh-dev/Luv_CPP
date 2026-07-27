e8f6fd3f933c419f3f56626a06983d1a10f425a7
suneetchugh-dev <valorise078gramming@gmail.com>
2026-07-28 03:39:28 +0530

    Learnt About Characters And ASCII Values

---
+++ Episode2_C++_Basics.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c = 'a';
+    char b = 'c';
+    cout << (int) c << endl; //typecasting c variable which is storing a character into integer datatype;
+
+    cout << c-b; //97-99 = -2
+}
---
+++ Practice/bits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printBinary(int num)
+{
+
+}
+
+int main()
+{
+    
+}
