12440b086e36e78abd5a9e5945b35c08b7f17b10
suneetchugh-dev <valorise078gramming@gmail.com>
2026-08-07 04:00:55 +0530

    Learnt About Scope, Types Of Loop, Pattern Printing

---
+++ Episode_4_ContionalStatements_Loops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int n;
+    // cin >> n;
+    // if(n%2==0)
+    // {
+    //     int x = 2;
+    // }
+    // cout << x; 
+    // This will cause a compilation error because x is declared inside the if block and is not accessible outside of it.
+    //scope in C++ is defined by curly braces {}. 
+    // Variables declared inside a block are only accessible within that block. In this case, the variable x is declared inside the if statement block, so it cannot be accessed outside of it. To fix this issue, you can declare x outside of the if block:
+
+    // int i = 1;
+    // while(i<=10)//conditions
+    // {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // for(int i=0; i<10; i++)
+    // {
+    //     cout << i << endl;
+    // }
+
+}
