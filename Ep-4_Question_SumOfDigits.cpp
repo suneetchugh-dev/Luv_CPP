addcbb46db24b56f04a20a33e9ae3c1cf99b058b
suneetchugh-dev <valorise078gramming@gmail.com>
2026-08-10 14:22:40 +0530

    learn how to run code for specific test cases, how to run infinite loop, and jump statements such as break and continue

---
+++ Ep-4_Question_SumOfDigits.cpp
@@ -0,0 +1,44 @@
+//you are given an integer n, you need to calculate the sum of n digits;
+//the first line contains integer T, representing the no of test cases
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    //running our code block for t test cases;
+
+    //using for loop (not preferred)
+    //using test case varible inside for loop waste a variable
+    // for(int test=0; test<t; ++t)
+    // {//your code block}
+
+    //so we instead while loop (preferred way)
+
+    // while(t--) //lopp will run till this condition become false
+    // {
+
+    // }
+
+
+    //how to run infinite loops
+    // while(1) //this condition will always be true
+    // {
+    //     cout << "Hello World";
+    // }
+
+
+    
+    // for(;;)
+    // {
+    //     cout << "Krish";
+    // }
+
+    return 0;
+}
--- Episode_4_ContionalStatements_Loops.cpp
+++ Episode_4_ContionalStatements_Loops.cpp
@@ -26,4 +26,53 @@ int main()
     //     cout << i << endl;
     // }
 
+
+    //Pattern Printing
+    // int n;
+    // cin >> n;
+    // for(int i=1; i<=n; i++) //For Line Number
+    // {
+    //     for(int j=1; j<=i; j++) //since according to line number we are printing stars
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+
+
+    //Jump Statements => break, continue
+
+    //break => breaks out of for loop after meeting specific condition
+    //continue => jumps out for that particular condition
+
+
+    //break
+    // for(int i=0; i<10; i++)
+    // {
+    //     if(i==8)
+    //     {
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         cout << i << endl;
+    //     }
+    // }
+
+
+
+    // cout << endl;
+    // //continue
+    // for(int i=0; i<10; i++)
+    // {
+    //     if(i==8)
+    //     {
+    //         continue;
+    //     }
+    //     else
+    //     {
+    //         cout << i << endl;
+    //     }
+    // }
 }
